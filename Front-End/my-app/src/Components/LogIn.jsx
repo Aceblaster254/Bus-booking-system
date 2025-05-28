cc1d5ddade407fb78782d8538c502a25c1851b78
Augasha <3higher04@gmail.com>
2025-05-28 09:39:49 +0300

    finishing login component

--- Front-End/my-app/src/Components/LogIn.jsx
+++ Front-End/my-app/src/Components/LogIn.jsx
@@ -5,6 +5,7 @@ import './LogIn.css'
 function Login({handleLogin}) {
   const [username, setUsername] = useState('');
   const [password, setPassword] =useState('')
+  const [adminNo, setAdminNo] = useState('')
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -25,6 +26,14 @@ function Login({handleLogin}) {
             <br />
           <div className='btn'>
             <button type="submit">LOGIN</button>
+
+            <button type="submit">ADMIN LOGIN
+                <div className='form-'>
+            <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required/>
+            <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+            <input type="text, password" placeholder="Admin No" value={adminNo} onChange={(e) => setAdminNo(e.target.value)} required/>
+          </div>
+            </button>
           </div>
         </form>
     </div>
